0122c30f9ac4af0ec7dc73674bfc302f77ce56fb
bartoszb <bartosz.boron@maibornwolff.de>
2018-09-24 15:11:20 +0200

    making use of 'outerClass' definition

--- src/protoParsing.ts
+++ src/protoParsing.ts
@@ -2,8 +2,8 @@ import protobuf = require('protocol-buffers');
 import fs = require('fs');
 import {TextDecoder} from "util";
 
-export const encodeProto = (protoDefPath: string, attributes: {}) => {
+export const encodeProto = (protoDefPath: string, attributes: {}, outerClass: string) => {
     let messages = protobuf(fs.readFileSync(protoDefPath));
-    let buffer = messages.Test.encode(attributes['Test']);
+    let buffer = messages[outerClass].encode(attributes);
     return new TextDecoder('utf-8').decode(buffer);
 }
--- src/tests/protoParsing.spec.ts
+++ src/tests/protoParsing.spec.ts
@@ -4,15 +4,13 @@ import {encodeProto} from "../protoParsing";
 
 describe('PROTO parsing', () => {
 
-    it('adsfasdfa', () => {
+    it('can encode simple nested messages', () => {
         const result = encodeProto('src/tests/resources/proto/test.proto', {
-            Test: {
-                nested: {
-                    nestedText: 'hello'
-                },
-                text: 'world'
-            }
-        });
+            nested: {
+                nestedText: 'hello'
+            },
+            text: 'world'
+        }, 'Test');
         expect(result).to.be.equal('\n\u0007\n\u0005hello\u0012\u0005world');
     });
 });
