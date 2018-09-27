8d8ee425cb891f9f1e3800c9c9619630dda69501
lw-mcno <maciej.noszczynski@libreworks.pl>
2018-09-27 09:26:10 +0200

    Import statement resolution for protobuf definitions

--- src/protoParsing.ts
+++ src/protoParsing.ts
@@ -1,14 +1,13 @@
 import protobuf = require('protocol-buffers');
-import fs = require('fs');
-import {TextDecoder, TextEncoder} from "util";
+import resolveProtobuf = require('resolve-protobuf-schema');
 
 export const encodeProto = (protoDefPath: string, attributes: {}, outerClass: string): Buffer => {
-    let messages = protobuf(fs.readFileSync(protoDefPath));
+    let messages = protobuf(resolveProtobuf.sync(protoDefPath));
     let buffer = messages[outerClass].encode(attributes);
     return buffer;
 }
 
 export const decodeProto = (protoDefPath: string, outerClass: string, buffer: Buffer) => {
-    let protoMessages = protobuf(fs.readFileSync(protoDefPath));
+    let protoMessages = protobuf(resolveProtobuf.sync(protoDefPath));
     return protoMessages[outerClass].decode(buffer);
 }
