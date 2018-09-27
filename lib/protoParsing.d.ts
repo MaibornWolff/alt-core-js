fc962daf0440f4c5fbeb93913664693772965ebb
bartoszb <bartosz.boron@maibornwolff.de>
2018-09-27 14:46:28 +0200

    releasing bugfix for the import functionality of proto files: 1.2.8

--- lib/protoParsing.d.ts
+++ lib/protoParsing.d.ts
@@ -1,3 +1,4 @@
 /// <reference types="node" />
-export declare const encodeProto: (protoDefPath: string, attributes: {}, outerClass: string) => Buffer;
-export declare const decodeProto: (protoDefPath: string, outerClass: string, buffer: Buffer) => any;
+export declare function resolveImportPath(filename: string, importPath: string): string;
+export declare function encodeProto(protoDefPath: string, attributes: {}, outerClass: string): Buffer;
+export declare function decodeProto(protoDefPath: string, outerClass: string, buffer: Buffer): any;
