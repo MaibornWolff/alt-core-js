63e8eebe6c9997ed500cc263b751c4dad471905e
lw-mcno <maciej.noszczynski@libreworks.pl>
2018-09-27 12:48:15 +0200

    Relative-to-absolute path import path handling, test

--- src/protoParsing.ts
+++ src/protoParsing.ts
@@ -1,13 +1,43 @@
 import protobuf = require('protocol-buffers');
-import resolveProtobuf = require('resolve-protobuf-schema');
+import schema = require('protocol-buffers-schema')
+import fs = require('fs')
+import path = require('path')
 
-export const encodeProto = (protoDefPath: string, attributes: {}, outerClass: string): Buffer => {
-    let messages = protobuf(resolveProtobuf.sync(protoDefPath));
+function merge(a, b) {
+  a.messages = a.messages.concat(b.messages)
+  a.enums = a.enums.concat(b.enums)
+  return a
+}
+
+function readProtobuf(filename: string) {
+
+  var sch = schema(fs.readFileSync(filename, 'utf-8'))
+  var imports = [].concat(sch.imports || [])
+
+  imports.forEach(function(i) {
+    sch = merge(sch, readProtobuf(resolveImportPath(filename, i)))
+  })
+
+  return sch
+}
+
+export function resolveImportPath(filename: string, importPath: string): string {
+    let currentDir = path.dirname(filename)
+
+    while (!fs.existsSync(path.resolve(currentDir, importPath)) && (path.parse(currentDir).root !== currentDir)) {
+        currentDir = path.resolve(currentDir, "..")
+    }
+
+    return path.resolve(currentDir, importPath); 
+}
+
+export function encodeProto(protoDefPath: string, attributes: {}, outerClass: string): Buffer {
+    let messages = protobuf(readProtobuf(protoDefPath));
     let buffer = messages[outerClass].encode(attributes);
     return buffer;
 }
 
-export const decodeProto = (protoDefPath: string, outerClass: string, buffer: Buffer) => {
-    let protoMessages = protobuf(resolveProtobuf.sync(protoDefPath));
+export function decodeProto(protoDefPath: string, outerClass: string, buffer: Buffer) {
+    let protoMessages = protobuf(readProtobuf(protoDefPath));
     return protoMessages[outerClass].decode(buffer);
 }
