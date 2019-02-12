299a54c7471f2dff4d566bc03bcef2658ca2faa1
bartoszb <bartosz.boron@maibornwolff.de>
2019-02-12 05:56:55 +0100

    releasing new version 1.3.1

--- lib/model/RestAction.d.ts
+++ lib/model/RestAction.d.ts
@@ -10,10 +10,11 @@ declare class RestAction implements Action {
     method: string;
     restHead: any;
     data: Map<string, string>;
+    dataBinary: string;
     form: any;
     responseValidation: string[];
     variables: Map<string, string>;
-    constructor(name: string, actionDef: any, url: string, serviceName: string, restMethod?: any, restHeaders?: any, restData?: any, restForm?: any, validators?: any, vars?: any);
+    constructor(name: string, actionDef: any, url: string, serviceName: string, restMethod?: any, restHeaders?: any, restData?: any, restDataBinary?: any, restForm?: any, validators?: any, vars?: any);
     static fromTemplate(actionDef: any, template: RestAction): RestAction;
     private static loadData;
     invoke(scenario: Scenario): ActionCallback;
