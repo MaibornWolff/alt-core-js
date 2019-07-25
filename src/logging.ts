531fd72586e65742ac8af3a1beb853772b523082
Johannes Loher <johannes.loher@maibornwolff.de>
2019-07-25 12:12:23 +0200

    adjust logging format

--- src/logging.ts
+++ src/logging.ts
@@ -1,16 +1,28 @@
 import * as pad from 'pad';
-import { createLogger, format, transports, Logger } from 'winston';
+import { Logger, createLogger, format, transports } from 'winston';
 import { OUTPUT_DIR } from '.';
 
 const { combine, timestamp, printf } = format;
 
+const trim = (text: string, length: number): string =>
+    text.length > length ? `${text.substring(0, length - 1)}…` : text;
+
+const trimAndPadLeft = (text: string, length: number): string =>
+    pad(trim(text, length), length);
+
+const trimAndPadRight = (text: string, length: number): string =>
+    pad(length, trim(text, length));
+
 const myFormat = printf(
-    info =>
-        `${info.timestamp} [${pad(info.scenario || 'unknown', 30)}] [${pad(
-            info.action || 'unknown',
-            30,
-        )}] ${pad(`[${info.level.toUpperCase()}]`, 7)} #${
-            info.message.startsWith('#') ? info.message : ` ${info.message}`
+    (info): string =>
+        `${info.timestamp} | ${trimAndPadLeft(
+            info.scenario || 'no scenario',
+            15,
+        )} | ${trimAndPadLeft(
+            info.action || 'no action',
+            15,
+        )} | ${trimAndPadRight(`${info.level.toUpperCase()}`, 5)} | ${
+            info.message
         }`,
 );
 
