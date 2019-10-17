4d7a56ac4677a5ad79774235d1892d0af00dc6a9
stefanke <stefan.kendlbacher@maibornwolff.de>
2019-10-17 12:47:04 +0200

    added clientCertificatePath/clientCertificateKey to RestAction

--- src/tests/integration/rest/rest.spec.ts
+++ src/tests/integration/rest/rest.spec.ts
@@ -1,7 +1,11 @@
 import 'mocha';
 import * as Http from 'http';
+import * as Https from 'https';
 
 import { expect } from 'chai';
+import * as fs from 'fs';
+import * as path from 'path';
+
 import { runMultipleScenariosWithConfigAsync } from '../../../index';
 
 describe('Rest Action', () => {
@@ -11,58 +15,132 @@ describe('Rest Action', () => {
     const envConfigDir = `${integrationTestBasePath}environment`;
     const environment = 'config';
 
-    let server;
+    describe('Requests', () => {
+        let server;
 
-    before(() => {
-        const port = 8080;
+        before(() => {
+            const port = 8080;
 
-        const requestHandler = (request, response) => {
-            response.setHeader('Content-Type', 'application/json');
-            const responseBody = { code: 200 };
-            response.end(JSON.stringify(responseBody));
-        };
+            const requestHandler = (request, response) => {
+                response.setHeader('Content-Type', 'application/json');
+                const responseBody = { code: 200 };
+                response.end(JSON.stringify(responseBody));
+            };
 
-        server = Http.createServer(requestHandler);
-        server.listen(port);
+            server = Http.createServer(requestHandler);
+            server.listen(port);
+        });
+
+        after(() => {
+            server.close();
+        });
+
+        it('should successfully perform s1', async () => {
+            const scenarioPath = `${integrationTestBasePath}scenarios/s1-restExpectingJsonResponseToBeValid.yaml`;
+
+            const result = await runMultipleScenariosWithConfigAsync(
+                actionDir,
+                outDir,
+                envConfigDir,
+                {
+                    numberOfScenariosRunInParallel: 1,
+                    environmentNameToBeUsed: environment,
+                    drawDiagrams: false,
+                },
+                [scenarioPath],
+            );
+
+            expect(result).to.be.equal(true);
+        });
+
+        it('should fail perform s2', async () => {
+            const scenarioPath = `${integrationTestBasePath}scenarios/s2-restExpectingJsonResponseNotToBeValid.yaml`;
+
+            const result = await runMultipleScenariosWithConfigAsync(
+                actionDir,
+                outDir,
+                envConfigDir,
+                {
+                    numberOfScenariosRunInParallel: 1,
+                    environmentNameToBeUsed: environment,
+                    drawDiagrams: false,
+                },
+                [scenarioPath],
+            );
+
+            expect(result).to.be.equal(false);
+        });
     });
 
-    after(() => {
-        server.close();
-    });
+    describe('Requests with Client Certificate', () => {
+        let server;
 
-    it('should successfully perform s1', async () => {
-        const scenarioPath = `${integrationTestBasePath}scenarios/s1-restExpectingJsonResponseToBeValid.yaml`;
+        before(() => {
+            const port = 8080;
 
-        const result = await runMultipleScenariosWithConfigAsync(
-            actionDir,
-            outDir,
-            envConfigDir,
-            {
-                numberOfScenariosRunInParallel: 1,
-                environmentNameToBeUsed: environment,
-                drawDiagrams: false,
-            },
-            [scenarioPath],
-        );
+            const requestHandler = (request, response) => {
+                response.writeHead(request.client.authorized ? 200 : 401);
+                response.end();
+            };
+            const serverOptions: Https.ServerOptions = {
+                key: fs.readFileSync(
+                    path.join(__dirname, 'resources/certs/server_key.pem'),
+                    'utf-8',
+                ),
+                cert: fs.readFileSync(
+                    path.join(__dirname, 'resources/certs/server_cert.pem'),
+                    'utf-8',
+                ),
+                ca: fs.readFileSync(
+                    path.join(__dirname, 'resources/certs/server_cert.pem'),
+                    'utf-8',
+                ),
+                requestCert: true,
+                rejectUnauthorized: false,
+            };
 
-        expect(result).to.be.equal(true);
-    });
+            server = Https.createServer(serverOptions, requestHandler);
+            server.listen(port);
+        });
 
-    it('should fail perform s2', async () => {
-        const scenarioPath = `${integrationTestBasePath}scenarios/s2-restExpectingJsonResponseNotToBeValid.yaml`;
+        after(() => {
+            server.close();
+        });
 
-        const result = await runMultipleScenariosWithConfigAsync(
-            actionDir,
-            outDir,
-            envConfigDir,
-            {
-                numberOfScenariosRunInParallel: 1,
-                environmentNameToBeUsed: environment,
-                drawDiagrams: false,
-            },
-            [scenarioPath],
-        );
+        it('should successfully perform s3', async () => {
+            const scenarioPath = `${integrationTestBasePath}scenarios/s3-restExpectingClientToBeAuthorized.yaml`;
 
-        expect(result).to.be.equal(false);
+            const result = await runMultipleScenariosWithConfigAsync(
+                actionDir,
+                outDir,
+                envConfigDir,
+                {
+                    numberOfScenariosRunInParallel: 1,
+                    environmentNameToBeUsed: environment,
+                    drawDiagrams: false,
+                },
+                [scenarioPath],
+            );
+
+            expect(result).to.be.equal(true);
+        });
+
+        it('should successfully perform s4', async () => {
+            const scenarioPath = `${integrationTestBasePath}scenarios/s4-restExpectingClientToBeUnauthorized.yaml`;
+
+            const result = await runMultipleScenariosWithConfigAsync(
+                actionDir,
+                outDir,
+                envConfigDir,
+                {
+                    numberOfScenariosRunInParallel: 1,
+                    environmentNameToBeUsed: environment,
+                    drawDiagrams: false,
+                },
+                [scenarioPath],
+            );
+
+            expect(result).to.be.equal(false);
+        });
     });
 });
