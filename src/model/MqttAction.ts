5b6b9dd543e4005cb5143ee63c9ab3a970ecb833
Christopher Füseschi <christopher.fueseschi@maibornwolff.de>
2020-09-25 10:58:52 +0200

    code review feedback

--- src/model/MqttAction.ts
+++ src/model/MqttAction.ts
@@ -87,8 +87,8 @@ class MqttAction implements Action {
             | undefined,
         protoFile = actionDef.protoFile,
         protoClass = actionDef.protoClass,
-        invokeEvenOnFail = actionDef.invokeEvenOnFail ?? false,
-        allowFailure = actionDef.allowFailure ?? false,
+        invokeEvenOnFail = !!actionDef.invokeEvenOnFail,
+        allowFailure = !!actionDef.allowFailure,
         diagramConfiguration = actionDef.diagramConfiguration ?? {},
     ) {
         this.name = name;
