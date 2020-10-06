41d842abaf750b4042437d05a22c4772da6da807
Christopher Füseschi <christopher.fueseschi@maibornwolff.de>
2020-10-06 11:12:54 +0200

    add comment

--- src/model/error/UnexpectedNumberOfMessagesError.ts
+++ src/model/error/UnexpectedNumberOfMessagesError.ts
@@ -6,6 +6,7 @@ export class UnexpectedNumberOfMessagesError extends Error {
         super(
             `Received an unexpected number of messages: ${numberOfReceivedMessages} (expected: ${expectedNumberOfMessages})`,
         );
+        // https://github.com/Microsoft/TypeScript-wiki/blob/master/Breaking-Changes.md#extending-built-ins-like-error-array-and-map-may-no-longer-work
         Object.setPrototypeOf(this, new.target.prototype);
         this.name = UnexpectedNumberOfMessagesError.name;
     }
